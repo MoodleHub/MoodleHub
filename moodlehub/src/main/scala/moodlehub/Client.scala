f7a766ac9eb3afb3c9926db26e7383fa951a3990
Remi Coudert <remi.coudert@epfl.ch>
2017-11-11 16:27:26 +0100

    functional for get_site_info

--- moodlehub/src/main/scala/moodlehub/Client.scala
+++ moodlehub/src/main/scala/moodlehub/Client.scala
@@ -2,36 +2,74 @@ package moodlehub
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws._
 import play.api.libs.ws.ahc._
 
-import scala.concurrent.Future
-import scala.util.{Success, Failure}
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
 
 object Client {
   import DefaultBodyReadables._
   import scala.concurrent.ExecutionContext.Implicits._
 
-  def main(args: Array[String]): Unit = {
-    // Create Akka system for thread and streaming management
-    implicit val system: ActorSystem = ActorSystem()
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
+  private var stopped = false
 
-    // Create the standalone WS client
-    // no argument defaults to a AhcWSClientConfig created from
-    // "AhcWSClientConfigFactory.forConfig(ConfigFactory.load, this.getClass.getClassLoader)"
-    val wsClient = StandaloneAhcWSClient()
+  // Create Akka system for thread and streaming management
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-    val x = call(wsClient)
-      .andThen { case _ => wsClient.close() }
-      .andThen { case _ => system.terminate() }
+  // Create the standalone WS client
+  // no argument defaults to a AhcWSClientConfig created from
+  // "AhcWSClientConfigFactory.forConfig(ConfigFactory.load, this.getClass.getClassLoader)"
+  val wsClient = StandaloneAhcWSClient()
+
+  private final val BASE_URL = "https://moodle.epfl.ch/webservice/rest/server.php"
+  private final val FORMAT = "json"
+  private final val TOKEN = "6aca2ab143095b1e8498c6e8c3364898"
+
+  private final val GET_SITE_INFO_FUN = "core_webservice_get_site_info"
+
+  private final val BASE_REQUEST = s"$BASE_URL?moodlewsrestformat=$FORMAT&wstoken=$TOKEN&wsfunction="
+
+  def stop(): Unit = {
+    wsClient.close()
+    system.terminate()
+    stopped = true
   }
 
-  def call(wsClient: StandaloneWSClient): Future[Unit] = {
-    wsClient.url("http://www.google.com").get().map { response =>
-      val statusText: String = response.statusText
-      val body = response.body[String]
-      println(s"Got a response $statusText")
+  def getSiteInfo(): Unit = {
+    callFunction(GET_SITE_INFO_FUN).onComplete {
+      case Success(s) =>
+        val json = Json.parse(s)
+        println(json)
+      case Failure(e) => throw e
     }
+  }
+
+  private def callFunction(name: String, args: Map[String, String] = Map()): Future[String] = {
+
+    println(BASE_REQUEST +
+      name +
+      args.foreach{ arg =>
+        val argName = arg._1
+        val argValue = arg._2
+        s"&$argName&$argValue"
+      })
+
+    wsClient.url(
+      BASE_REQUEST +
+      name +
+        (if(args.nonEmpty) args.foreach{ arg =>
+        val argName = arg._1
+        val argValue = arg._2
+        s"&$argName&$argValue"
+      } else "")
+    ).get().map { response =>
+      response.body[String]
+    }.
+      andThen{case _ => wsClient.close()}.
+      andThen{case _ => system.terminate()}
   }
 }

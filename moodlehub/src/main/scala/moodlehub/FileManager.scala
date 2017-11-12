30f5338209ebbc73fa2fa36980b0dd6ee5f339d1
Leonardo Aoun <aounleo@tsf-428-wpa-1-027.epfl.ch>
2017-11-12 03:10:28 +0100

    implement the synchronizer

--- moodlehub/src/main/scala/moodlehub/FileManager.scala
+++ moodlehub/src/main/scala/moodlehub/FileManager.scala
@@ -8,28 +8,51 @@ import play.api.libs.json._
 
 object FileManager {
 
-  def downloadFile(url: String, filename: Path)(implicit token: Token) = {
+  val TIMESTAMPS = ".timestamps.json"
+
+  def downloadFile(url: String, filename: Path)(implicit token: Token): Unit = {
     new URL(s"$url&token=${token.token}") #> new File(filename.path) !!
   }
 
-  def createDirectory(path: Path) = {
+  def createDirectory(path: Path): Unit = {
     new File(path.path).mkdir()
   }
 
   def getTimeStamps(section: Section): JsObject = {
-    val tsPath = section.coursePath add ".timestamps.json"
+    val tsPath = section.path add TIMESTAMPS
     val file = new File(tsPath.path)
-    if(! file.exists) {
+    if(!file.exists) {
       // create dummy file
-      instantiateTimeStamps(tsPath)
+      writeTimeStamps(tsPath, Json.stringify(new JsObject(Map())))
     }
     val in = new FileInputStream(file)
     Json.parse(in).as[JsObject]
   }
 
-  private def instantiateTimeStamps(path: Path) = {
-    val json = Json.stringify(new JsObject(Map()))
+  private def writeTimeStamps(path: Path, json: String): Unit = {
     val out = new FileOutputStream(new File(path.path))
     out.write(json.getBytes)
+  }
+
+  /**
+    *
+    * @param section
+    * @param filename
+    * @param stamp
+    * @return true iff the timestamp was modified
+    */
+  def addTimeStamp(section: Section, filename: String, stamp: Int): Boolean = {
+    val json = getTimeStamps(section)
+    val tsPath = section.path add TIMESTAMPS
+    val timeStamp = json.value.get(filename) match {
+      case Some(jsValue) => jsValue.as[Int]
+      case None => -1
+    }
+    val ret = stamp > timeStamp
+    if(ret) {
+      val jonSnow = JsObject(json.value + (filename -> JsNumber(stamp)))
+      writeTimeStamps(tsPath, Json.stringify(jonSnow))
+    }
+    ret
   }
 }
--- moodlehub/src/main/scala/moodlehub/Section.scala
+++ moodlehub/src/main/scala/moodlehub/Section.scala
@@ -7,25 +7,42 @@ class Section(token: Token, val coursePath: Path, obj: JsObject) {
   private val value = obj.value
   private val name = value("name").as[String]
 
-  private val path: Path = coursePath add util.formatString(name)
+  val path: Path = coursePath add util.formatString(name)
   FileManager.createDirectory(path)
   private val summary = value("summary").as[String]
   private val modules = value("modules").as[JsArray].value
 
-  private val fileModules: List[Map[String, JsValue]] = modules.filter(_.as[JsObject].value("modplural").as[String] == "Files").map(_.as[JsObject].value).toList
+  private val fileModules: List[Map[String, JsValue]] =
+    modules.filter(_.as[JsObject].value("modplural").as[String] == "Files")
+      .map(_.as[JsObject].value).toList
 
   fileModules.foreach { module =>
     val contents = module("contents").as[JsArray].value
     contents.foreach { file =>
       val fileMap = file.as[JsObject].value
       if(fileMap("type").as[String] == "file") {
-        val filename = path add fileMap("filename").as[String]
-        val fileurl = fileMap("fileurl").as[String]
-        FileManager.downloadFile(fileurl, filename)(token)
+        processFileMap(fileMap)
       }
     }
   }
+
+  private def processFileMap(fileMap: Map[String, JsValue]): Unit = {
+    val filename = fileMap("filename").as[String]
+    val filepath = path add filename
+    val fileurl = fileMap("fileurl").as[String]
+
+    val filesize = fileMap("filesize").as[Int]
+    val lastMod = fileMap("timemodified").as[Int]
+
+    val change = FileManager.addTimeStamp(this, filename, lastMod)
+    if(change){
+      FileManager.downloadFile(fileurl, filepath)(token)
+    }
+  }
+
 }
+
+
 
 object Section {
   def apply(obj: JsObject)(implicit token: Token, path: Path): Section =
